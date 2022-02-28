a7a6d978abac91f90b3faf07c28ca9ecbdffe6b7
unknown <mstuhin>
2022-02-28 19:42:46 +0600

    java

--- java.js
+++ java.js
@@ -3,8 +3,11 @@ const allPlayers = () => {
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
     fetch(url)
         .then(res => res.json())
-        .then((data) => console.log(data));
-    console.log(url);
+        .then((data) => showPlayersDetails(data));
 
-    console.log(searchValue);
-}
+
+};
+
+const showPlayersDetails = (player) => {
+    console.log(player);
+} 
